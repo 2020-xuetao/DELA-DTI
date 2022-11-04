9280527716fc162b2ac0756252449f12d7d2e3b1
taoxue <73324461+2020-xuetao@users.noreply.github.com>
2022-11-04 21:52:04 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,132 @@
+import os
+import numpy as np
+from math import sqrt
+from scipy import stats
+from torch_geometric.data import InMemoryDataset, DataLoader
+from torch_geometric import data as DATA
+import torch
+from datetime import datetime
+
+class TestbedDataset(InMemoryDataset):
+    def __init__(self, root='/tmp', dataset='davis', 
+                 xd=None, xt=None, y=None, transform=None,
+                 pre_transform=None,smile_graph=None):
+
+        #root is required for save preprocessed data, default is '/tmp'
+        super(TestbedDataset, self).__init__(root, transform, pre_transform)
+        # benchmark dataset, default = 'davis'
+        self.dataset = dataset
+        if os.path.isfile(self.processed_paths[0]):
+            print('Pre-processed data found: {}, loading ...'.format(self.processed_paths[0]))
+            self.data, self.slices = torch.load(self.processed_paths[0])
+        else:
+            print('Pre-processed data {} not found, doing pre-processing...'.format(self.processed_paths[0]))
+            self.process(xd, xt, y, smile_graph)
+            self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        pass
+        #return ['some_file_1', 'some_file_2', ...]
+
+    @property
+    def processed_file_names(self):
+        return [self.dataset + '.pt']
+
+    def download(self):
+        # Download to `self.raw_dir`.
+        pass
+
+    def _download(self):
+        pass
+
+    def _process(self):
+        if not os.path.exists(self.processed_dir):
+            os.makedirs(self.processed_dir)
+
+    # Customize the process method to fit the task of drug-target affinity prediction
+    # Inputs:
+    # XD - list of SMILES, XT: list of encoded target (categorical or one-hot),
+    # Y: list of labels (i.e. affinity)
+    # Return: PyTorch-Geometric format processed data
+    def process(self, xd, xt, y, smile_graph):
+        assert (len(xd) == len(xt) and len(xt) == len(y)), "The three lists must be the same length!"
+        data_list = []
+        data_len = len(xd)
+        for i in range(data_len):
+            print('Converting SMILES to graph: {}/{}'.format(i+1, data_len))
+            smiles = xd[i]
+            target = xt[i]
+            labels = y[i]
+            # convert SMILES to molecular representation using rdkit
+            c_size, features, edge_index = smile_graph[smiles] # 节点，节点特征，边
+            # make the graph ready for PyTorch Geometrics GCN algorithms:
+            GCNData = DATA.Data(x=torch.Tensor(features),
+                                edge_index=torch.LongTensor(edge_index).transpose(1, 0),
+                                y=torch.FloatTensor([labels]))
+            GCNData.target = torch.Tensor([target])          
+            GCNData.__setitem__('c_size', torch.LongTensor([c_size]))
+            # append graph, label and target sequence to data list
+            data_list.append(GCNData)
+            # print(data_list)
+            # break
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+        print('Graph construction done. Saving to file.')
+        data, slices = self.collate(data_list) # slice是分割方式
+        # save preprocessed data:
+        torch.save((data, slices), self.processed_paths[0])
+        # torch.save(obj,f) obj是要存储的对象，f是存储的路径
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, savepath = None, patience=7, verbose=False, delta=0, num_n_fold = 0):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.num_n_fold = num_n_fold
+        self.savepath = savepath
+        self.current_time = datetime.now().strftime('%b%d_%H-%M-%S')
+
+    def __call__(self, val_loss, model, num_epoch):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model,num_epoch)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model,num_epoch)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model,num_epoch):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        # torch.save(model.state_dict(), '.\output\model\checkpoint%d.pt' % num_epoch )
+        torch.save(model.state_dict(), self.savepath + '/valid_best_checkpoint.pth')
+        self.val_loss_min = val_loss
